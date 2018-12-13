bc4c6b3a8dc0e1ed0dd836e93ccf331b2f0052ba
qiansc <van3008@163.com>
2018-12-13 13:42:02 +0800

    chore:add docs

--- README.md
+++ README.md
@@ -5,8 +5,39 @@
 [![npm package](https://img.shields.io/npm/v/ralltiir-skeleton-tombstone.svg)](https://www.npmjs.org/package/ralltiir-skeleton-tombstone)
 [![npm downloads](http://img.shields.io/npm/dm/ralltiir-skeleton-tombstone.svg)](https://www.npmjs.org/package/ralltiir-skeleton-tombstone)
 
-# What's ralltiir-skeleton-tombstone
-Ralltiir/ralltiir-skeleton-tombstone
+## What's ralltiir-skeleton-tombstone
+ralltiir-skeleton-tombstone是提供了ralltiir-skeleton所需的骨架屏类外观库，实现上有基于SearchUI及原生（非SearchUI）两种方式。
 
-# Demo
-[ralltiir-skeleton-tombstone](https://ralltiir.github.io/ralltiir-skeleton-tombstone/)
+## Demo
+[ralltiir-skeleton-tombstone](https://ralltiir.github.io/ralltiir-skeleton-tombstone/demo/)
+
+## Module
+
+[API](https://ralltiir.github.io/ralltiir-skeleton-tombstone/)
+
+### SearchUI Tombstone
+
+Example
+
+```
+require([
+ 'ralltiir-skeleton',
+ 'ralltiir-skeleton-tombstone',
+ 'Atom',
+ 'search-ui/v2/Tombstone/Tombstone',
+ 'search-ui/v2/Tombstone/ImgTombstone.css'
+], function(Skeleton, Tombstone, Atom, TombstoneUI) {
+     var appearance = new Tombstone.SearchTombstone({
+       Atom: Atom,
+       TombstoneUI: TombstoneUI
+     });
+     skeleton = new Skeleton.Skeleton(container, appearance);
+     skeleton.create();
+});
+
+```
+
+### 原生 Tombstone
+
+敬请期待
+
--- src/searchTombstone.ts
+++ src/searchTombstone.ts
@@ -2,7 +2,7 @@
  * @Author: qiansc
  * @Date: 2018-12-12 11:28:18
  * @Last Modified by: qiansc
- * @Last Modified time: 2018-12-13 10:56:30
+ * @Last Modified time: 2018-12-13 13:40:28
  */
 
 declare type Atom = any;
@@ -10,9 +10,28 @@ declare type TombstoneUI = any;
 
 import { SkeletonAppearance, SkeletonAppearanceOptions } from "ralltiir-skeleton";
 
+/**
+ * 使用SearchUI/Tombstone组件实现的的SkeletonAppearance
+ *
+ * @example
+ * require([
+ *  'ralltiir-skeleton',
+ *  'ralltiir-skeleton-tombstone',
+ *  'Atom',
+ *  'search-ui/v2/Tombstone/Tombstone',
+ *  'search-ui/v2/Tombstone/ImgTombstone.css'
+ * ], function(Skeleton, Tombstone, Atom, TombstoneUI) {
+ *      var appearance = new Tombstone.SearchTombstone({
+ *        Atom: Atom,
+ *        TombstoneUI: TombstoneUI
+ *      });
+ *      skeleton = new Skeleton.Skeleton(container, appearance);
+ *      skeleton.create();
+ * });
+ */
+
 export class SearchTombstone implements SkeletonAppearance {
   private container: HTMLElement;
-  // private id = new Date().getTime().toString(32);
   constructor(private options: SearchTombstoneOptions) {}
   public render(options: SkeletonAppearanceOptions) {
     this.container = options.target;
@@ -42,8 +61,16 @@ export class SearchTombstone implements SkeletonAppearance {
 }
 
 interface SearchTombstoneOptions {
+  /** 依赖的Atom库 */
   Atom: Atom;
+  /** SearchUI Tombstone组件 */
   TombstoneUI: TombstoneUI;
+  /** 边距设置 */
   margin?: string;
+  /** 样式类型设置 */
   type?: number;
+  /** 图片占位图占的屏幕数量；默认1屏；当且仅当为图片类型 type === 1 时生效 */
+  imgNum?: number;
+  /** 其他类型占位加载的条数；默认6条；当且仅当不为图片类型 type !== 1 时生效 */
+  num?: number;
 }
